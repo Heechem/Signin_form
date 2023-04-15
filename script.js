cfd6b4e5c4674f398ff4f8fd26c6bc316c3948c2
Heechem <hichempeter@gmail.com>
2023-04-15 09:21:03 -0600

    adding JS validation

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const small = document.querySelector(".msg");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("Con_password");
-const button = document.querySelector("button");
+const small = document.querySelector('.msg');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('Con_password');
+const button = document.querySelector('button');
+const inputs = document.querySelectorAll('input');
+const errorContainer = document.querySelector('.error_message');
 
 console.log(password);
 
 function errorMsg() {
   if (password.value.length === 0) {
-    password.classList.add("invalid");
-    small.style.display = "block";
+    password.classList.add('invalid');
+    password.setCustomValidity('Please enter an email here');
+    small.style.display = 'block';
   } else {
-    small.style.display = "none";
-    password.classList.remove("invalid");
+    small.style.display = 'none';
+    password.classList.remove('invalid');
   }
 }
 
-button.addEventListener("click", () => {
-  event.preventDefault();
+button.addEventListener('click', (e) => {
+  e.preventDefault();
   errorMsg();
+  validationField();
 });
+
+function validationField() {
+  inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      input.classList.add('invalid');
+      errorContainer.classList.add('visible');
+    }
+  });
+}
